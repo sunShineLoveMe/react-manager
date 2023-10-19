fc26fbda33a4c8ff2b221fa00d055a3e48b5a875
sunShineLoveMe <juneandrao@outlook.com>
2023-10-19 22:16:50 +0800

    配置项目环境

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  // 箭头函数参数，如果是一个参数，去掉括号
+  arrowParens: 'avoid',
+  // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
+  bracketSpacing: true,
+  // 尾随逗号
+  trailingComma: 'none',
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+let list = [1, 3, 5].map(i => i + 1)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />

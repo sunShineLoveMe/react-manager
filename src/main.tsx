753abc23eb67b7561fac7fd2ab9cc44fd01c1e1d
sunShineLoveMe <juneandrao@outlook.com>
2023-10-22 21:17:42 +0800

    推送分支

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function ReactDemo() {
+  return <h2>欢迎学习React课程</h2>
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes></Routes>
+  </BrowserRouter>,
 )

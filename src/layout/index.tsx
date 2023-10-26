f2efd4fc21c87814ba83ec60f750ae4f005654d8
sunShineLoveMe <juneandrao@outlook.com>
2023-10-26 09:27:52 +0800

    水印的使用

---
+++ src/layout/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { Layout, Menu, theme, Watermark } from 'antd'
+
+const { Header, Content, Footer, Sider } = Layout
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
+
+  return (
+    <Watermark content='栉云版权'>
+      <Layout>
+        <Sider
+          breakpoint='lg'
+          collapsedWidth='0'
+          onBreakpoint={broken => {
+            console.log(broken)
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type)
+          }}
+        >
+          <div className='demo-logo-vertical' />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>顶部区域</Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    </Watermark>
+  )
+}
+
+export default App
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,15 +3,25 @@ import Login from '@/views/login/Login'
 import Welcome from '@/views/Welcome'
 import Error403 from '@/views/403'
 import NotFound from '@/views/404'
+import Layout from '@/layout'
 
 const router = [
   {
     path: '/',
-    element: <Welcome />,
+    element: <Navigate to='/welcome' />,
   },
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/welcome',
+        element: <Welcome />,
+      },
+    ],
   },
   {
     path: '*',
